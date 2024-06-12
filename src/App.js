ee6df587ccde470d47f0b9e06b91e4e20dfd7e7b
notaskhalsa <askhalsa29@gmail.com>
2024-06-12 09:29:18 -0400

    product list and details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import AppRoutes from "./routes/AppRoutes.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </div>
   );
 }
